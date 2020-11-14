5989326f6be44cdeb77fd44e88786554cb973be8
Anna Tran <annatran@annas-air.mynet>
2020-11-14 12:35:02 +0000

    add package metadata information

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='units_converter',
+    version='1.0',
+    scripts=['app.py'],
+    author='Anna Tran',
+    description='A units converter package',
+    packages=setuptools.find_packages()
+)
