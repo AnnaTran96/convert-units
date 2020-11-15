d596b551a05b5f7dc11938a60587b153a9eae162
Anna Tran <annatran@Annas-MacBook-Air.local>
2020-11-15 16:16:34 +0000

    test value error exception

--- test_app.py
+++ test_app.py
@@ -1,5 +1,7 @@
-from convert_units.app import run_script
+from convert_units.app import run_script, unit_convert
 import io
+import pytest
+from _pytest.outcomes import Failed
 
 def test_unit_convert(monkeypatch):
     test_input = "5 m in cm"
@@ -7,3 +9,8 @@ def test_unit_convert(monkeypatch):
     model_answer = "500.0cm"
     assert run_script(True) == model_answer
     
+# def test_value_error(monkeypatch):
+#     string_input = '5m in cm'
+#     with pytest.raises(ValueError) as e:
+#         monkeypatch.setattr('sys.stdin', io.StringIO(string_input))
+#     assert str(e.value) == "ValueError: invalid literal for int() with base 10: '5m'"
